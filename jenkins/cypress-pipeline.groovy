b943a2ffe9e25c8443e9b2a840f7f91900b3bfa9
Houssainy <mohamed.houssainy@gmail.com>
2019-11-08 18:56:49 +0200

    Test.

--- jenkins/cypress-pipeline.groovy
+++ jenkins/cypress-pipeline.groovy
@@ -1,27 +1,30 @@
 def meteorRunner = "meteor"
 def cypressLabel = "cypress"
 
-def cypressStages = [:]
 
-for (i = 0; i < 3; i++) {
-  cypressStages["Agen - ${i}"] = {
-    stage("Agent - ${i}") {
-      agent { label cypressLabel }
-      steps {
-        sh "cd \${WORKSPACE}"
-        sh "export PATHABLE_NEXT_HOME=\${WORKSPACE}"
-        sh "./scripts/db/reimport.sh staging test-data datasets"
-        sh "./scripts/db/fix-community-domains.sh staging"
-        sh "${meteorRunner} yarn cypress:run"
+def map = [
+        Bob  : 42,
+        Alice: 54,
+        Max  : 33
+]
+
+pipeline {
+  agent none
+
+  stages {
+    stage("Parallel stages") {
+      parallel {
+        node {
+            map.each { entry ->
+                stage (entry.key) {
+                    timestamps{
+                        echo "$entry.value"
+                    }
+                }
+            }
+        }
       }
     }
   }
 }
 
-
-System.out.println("HELLOOOOOOOOOOOOOOOOOoo")
-
-System.out.println(cypressLabel)
-
-
-System.out.println(cypressStages)
