07e54b84a3fb3b5e4d6ccecd9c47d7687e6baea4
Houssainy <mohamed.houssainy@gmail.com>
2019-11-09 01:00:36 +0200

    Test.

--- jenkins/cypress-pipeline.groovy
+++ jenkins/cypress-pipeline.groovy
@@ -1,27 +1,31 @@
 def meteorRunner = "meteor"
 def cypressLabel = "cypress"
 
-
-def map = [
-        Bob  : 42,
-        Alice: 54,
-        Max  : 33
-]
-
 pipeline {
   agent none
 
   stages {
     stage("Parallel stages") {
       parallel {
-        node {
-            map.each { entry ->
-                stage (entry.key) {
-                    timestamps{
-                        echo "$entry.value"
-                    }
-                }
-            }
+        stage("Agent - 1") {
+          agent { label cypressLabel }
+          steps {
+            sh "cd \${WORKSPACE}"
+            sh "export PATHABLE_NEXT_HOME=\${WORKSPACE}"
+            sh "./scripts/db/reimport.sh staging test-data datasets"
+            sh "./scripts/db/fix-community-domains.sh staging"
+            sh "${meteorRunner} yarn cypress:run"
+          }
+        }
+        stage("Agent - 2") {
+          agent { label cypressLabel }
+          steps {
+            sh "cd \${WORKSPACE}"
+            sh "export PATHABLE_NEXT_HOME=\${WORKSPACE}"
+            sh "./scripts/db/reimport.sh staging test-data datasets"
+            sh "./scripts/db/fix-community-domains.sh staging"
+            sh "${meteorRunner} yarn cypress:run"
+          }
         }
       }
     }
