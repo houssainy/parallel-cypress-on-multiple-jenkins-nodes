ec3e44e2562e1f35f7b0f20e7d77d0d035b74b18
Houssainy <mohamed.houssainy@gmail.com>
2019-11-08 18:40:09 +0200

    Test.

--- jenkins/cypress-pipeline.groovy
+++ jenkins/cypress-pipeline.groovy
@@ -7,19 +7,21 @@ pipeline {
   stages {
     stage("Parallel stages") {
       parallel {
-
-        for (i = 0; i <3; i++) {
-          stage("Agent - ${i}") {
-            agent { label cypressLabel }
-            steps {
-              sh "cd \${WORKSPACE}"
-              sh "export PATHABLE_NEXT_HOME=\${WORKSPACE}"
-              sh "./scripts/db/reimport.sh staging test-data datasets"
-              sh "./scripts/db/fix-community-domains.sh staging"
-              sh "${meteorRunner} yarn cypress:run"
+        script {
+          for (i = 0; i <3; i++) {
+            stage("Agent - ${i}") {
+              agent { label cypressLabel }
+              steps {
+                sh "cd \${WORKSPACE}"
+                sh "export PATHABLE_NEXT_HOME=\${WORKSPACE}"
+                sh "./scripts/db/reimport.sh staging test-data datasets"
+                sh "./scripts/db/fix-community-domains.sh staging"
+                sh "${meteorRunner} yarn cypress:run"
+              }
             }
           }
         }
+
       }
     }
   }
