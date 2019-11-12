2affb8165fba6cc246ee50e1cc4d26af970a2948
Houssainy <mohamed.houssainy@gmail.com>
2019-11-12 21:20:56 +0200

    dd

--- jenkins/cypress-pipeline.groovy
+++ jenkins/cypress-pipeline.groovy
@@ -3,28 +3,24 @@ def cypressLabel = "cypress"
 
 def numberOfInstances = 3
 
+def buildStages = [:]
+for (int i = 0; i < numberOfInstances; i++) {
+  buildStages["Agent - ${i}"] = {
+    agent {label cypressLabel }
+
+    stage("kokokokok ${i}") {
+      sh 'ifconfig'
+      sh 'date'
+    }
+  }
+}
+
 pipeline {
   agent none
 
   stages {
     stage("Parallel stages") {
-
-      steps {
-        script {
-            def buildStages = [:]
-            for (int i = 0; i < numberOfInstances; i++) {
-              buildStages["Agent - ${i}"] = {
-                agent {label cypressLabel }
-                stage("kokokokok ${i}") {
-                  sh 'ifconfig'
-                  sh 'date'
-                }
-              }
-            }
-
-            parallel buildStages
-        }
-      }
+      parallel buildStages
     }
   }
 }
