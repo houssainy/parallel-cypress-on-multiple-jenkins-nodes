dd350d700d47e46aa60b1110a4daed8dda5579ea
Houssainy <mohamed.houssainy@gmail.com>
2019-11-13 14:08:02 +0200

    temp.

--- jenkins/cypress-pipeline.groovy
+++ jenkins/cypress-pipeline.groovy
@@ -12,7 +12,7 @@ numberOfInstances = 3
 
 // Declirative Pipeline Script
 pipeline {
-  agent {label "cypress3"} // Agents will be specified in the parallel stages
+  agent { label "cypress4" } // Agents will be specified in the parallel stages
 
   stages {
     // First stage is to run db scripts to re-import the test datasets
@@ -24,61 +24,4 @@ pipeline {
       }
     }
   }
-}
-
-//
-// Helper functions
-//
-
-/**
- * return n stages directives where n is defined by
- * `numberOfInstances` variable.
- *
- * Each stage run db reimport scripts on cyprss${i} node.
- */
-def getBuildStages() {
-  def buildStages = [:]
-  for (int i = 1; i <= numberOfInstances; i++) {
-    int currentIndex = i
-
-    buildStages["Agent - ${currentIndex}"] = {
-      node("master") {
-        stage("Agent - ${currentIndex}") {
-
-          sh "echo ${answer}"
-          sh "echo ${installYarn}"
-          sh "${installYarn}"
-        }
-      }
-    }
-  }
-
-  return buildStages
-}
-
-/**
- * return n stages directives where n is defined by
- * `numberOfInstances` variable.
- *
- * Each stage installs the dependencies in jenkins workspace
- * then it runs the cypress test using --parallel flag.
- *
- * Nodes selection is done by Jenkins where all the stages
- * are just targeting the nodes with label `cypress` only.
- */
-def getCypressTestStages() {
-  def cypressStages = [:]
-  for (int i = 1; i <= numberOfInstances; i++) {
-    int currentIndex = i
-
-    cypressStages["Agent - ${currentIndex}"] = {
-      node("${cypressLabel}") {
-        stage("Agent - ${currentIndex}") {
-          sh "meteor yarn cypress:run --parallel"
-        }
-      }
-    }
-  }
-
-  return cypressStages
 }
